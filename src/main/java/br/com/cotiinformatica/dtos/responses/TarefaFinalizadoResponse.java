77981b656ed701b798ba1d93441abb18f4b8e6a9
DiegooQuaresma <diegoo.quaresmaa@gmail.com>
2025-09-24 19:13:21 -0300

    first commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/responses/TarefaFinalizadoResponse.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.dtos.responses;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class TarefaFinalizadoResponse {
+
+	private String status;
+	private Integer quantidade;
+
+}
